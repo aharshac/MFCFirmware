47656f05950d52a93efed35c5602b8df6770c678
aharshac <aharshac@gmail.com>
2016-05-06 23:24:09 +0530

    Added MFC mode constants and sensitive 3DP-only codes

---
+++ MFC.h
@@ -0,0 +1,17 @@
+/**
+ * MFC.h
+ */
+ 
+ #ifndef MFC_H
+#define MFC_H
+
+#define TOOL_MODE_3DP 0
+#define TOOL_MODE_LASER 1
+#define TOOL_MODE_MILL 2
+
+
+#define SENSITIVE_3DP_M { 104, 109, \
+    140, 145, 190, 600	\
+}
+
+#define SENSITIVE_3DP_G { 10, 11}
